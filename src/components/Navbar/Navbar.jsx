955559203e91cdb5f58741446a96ae3bd191c475
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-01 18:07:44 -0600

    Navbar added with desktop styles. 1440px design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
-import './Navbar.scss';
+import { navLinks } from "../../constants";
+import { avatar } from "../../assets";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='nav'>Navbar</div>
-  )
-}
+    <nav className="nav">
+      <div className="nav__logo">
+        <p>Lenscop</p>
+      </div>
+      <ul className="nav__links">
+        {navLinks.map((link, index) => (
+          <li key={`nav-bar-links-${index}`}>
+            <a className="nav__link" href={link.route}>
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="nav__avatar">
+        <img src={avatar} alt="avatar" />
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

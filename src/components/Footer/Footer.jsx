2ecc6b2cacfaab7d687d47eb7bf7449d7a95d73f
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-02 14:32:50 -0600

    Footer section added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,28 @@
 import "./Footer.scss";
+import {
+  be,
+  email,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+} from "../../assets";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <h2>Contact Us</h2>
+      <div className="images">
+        <img src={be} alt="be" />
+        <img src={email} alt="email" />
+        <img src={facebook} alt="facebook" />
+        <img src={instagram} alt="instagram" />
+        <img src={linkedin} alt="linkedin" />
+        <img src={twitter} alt="twitter" />
+      </div>
+      <p>&copy; 2023 Lenscop Media.</p>
+    </div>
+  );
 };
 
 export default Footer;

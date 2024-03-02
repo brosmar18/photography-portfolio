7d40e31aa3e23edbcf98cf320875fcb7ba5d487c
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-02 14:00:07 -0600

    Latest project gallery section added. Desktop 1440px styles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,13 +11,13 @@ const Projects = () => {
         </p>
       </div>
       <div className="projects__grid">
-        <div className="box-1">Box 1</div>
-        <div className="box-2">Box 2</div>
-        <div className="box-3">Box 3</div>
-        <div className="box-4">Box 4</div>
-        <div className="box-5">Box 5</div>
-        <div className="box-6">Box 6</div>
-        <div className="box-7">Box 7</div>
+        <div className="box-1" />
+        <div className="box-2" />
+        <div className="box-3" />
+        <div className="box-4" />
+        <div className="box-5" />
+        <div className="box-6" />
+        <div className="box-7" />
       </div>
     </div>
   );
